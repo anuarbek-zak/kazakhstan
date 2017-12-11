738948ff83c0c8682d7b083de253173dcb3c2a7c
Anuarbek Zakiryanov <anuarbekzakirianov97@gmail.com>
2017-12-11 22:42:39 +0600

    2

--- frontend/glavnaya/glavnaya.js
+++ frontend/glavnaya/glavnaya.js
@@ -1,5 +1,20 @@
 angular.module('myApp').controller('glavnayaCtrl',function ($http,$state,$localStorage) {
   var vm = this;
- 	vm.langs = {'eng':'Eglish','rus':'Русский','kaz':'Қазақ'}
- 	vm.currentLang = 'eng'
+ 	vm.langs = {'eng':'English','rus':'Русский','kaz':'Қазақ'}
+ 	vm.currentLang = 'eng';
+
+ 	$http.get('glavnaya/lang.json')
+ 		.success(function(data) {
+ 			vm.langsTexts = data;
+ 			vm.currentText = vm.langsTexts[vm.currentLang];
+ 			console.log(vm.currentText)
+ 		})
+ 		.error(function(err) {
+ 				console.log(err)
+ 		})
+
+ 	vm.changeLang = function(name) {
+ 		vm.currentText = vm.langsTexts[name];
+ 		vm.currentLang = name;
+ 	}
 });
