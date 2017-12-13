e81bfa38682b0884da51d4b4a9907d1d12380a38
Anuarbek Zakiryanov <anuarbekzakirianov97@gmail.com>
2017-12-14 03:17:57 +0600

    commit

--- frontend/commonJS/index.module.js
+++ frontend/commonJS/index.module.js
@@ -1,2 +1,2 @@
-angular.module('myApp', ['ngAnimate','ngFileUpload','ui.router','ngStorage','mgcrea.ngStrap'])
+angular.module('myApp', ['ngAnimate','ui.router','ngStorage','mgcrea.ngStrap'])
 
--- frontend/glavnaya/glavnaya.js
+++ frontend/glavnaya/glavnaya.js
@@ -1,20 +1,41 @@
 angular.module('myApp').controller('glavnayaCtrl',function ($http,$state,$localStorage) {
-  var vm = this;
- 	vm.langs = {'eng':'English','rus':'Русский','kaz':'Қазақ'}
- 	vm.currentLang = 'eng';
+	var vm = this;
+	vm.langs = {'eng':'English','rus':'Русский','kaz':'Қазақ'}
+	vm.currentLang = 'eng';
+	vm.slides = [{src:'../assets/img/expo1.jpg'},{src:'../assets/img/expo2.jpg'},{src:'../assets/img/expo3.jpg'}]
 
- 	$http.get('glavnaya/lang.json')
- 		.success(function(data) {
- 			vm.langsTexts = data;
- 			vm.currentText = vm.langsTexts[vm.currentLang];
- 			console.log(vm.currentText)
- 		})
- 		.error(function(err) {
- 				console.log(err)
- 		})
 
- 	vm.changeLang = function(name) {
- 		vm.currentText = vm.langsTexts[name];
- 		vm.currentLang = name;
- 	}
+	$http.get('glavnaya/lang.json')
+	.success(function(data) {
+		vm.langsTexts = data;
+		vm.currentText = vm.langsTexts[vm.currentLang];
+	})
+	.error(function(err) {
+		console.log(err)
+	})
+
+	vm.changeLang = function(name) {
+		vm.currentText = vm.langsTexts[name];
+		vm.currentLang = name;
+	}
+
+		$('.slider').carousel({
+
+  // the number of images to display
+  num: 5, 
+
+  // max width of the active image
+  maxWidth: 600,
+
+  // min width of the active image
+  maxHeight: 400, 
+
+  autoPlay: false,
+  // 0.0 - 1.0
+  scale: 0.8,
+  // the distance between images
+  distance: 120
+  
+});
+	
 });
