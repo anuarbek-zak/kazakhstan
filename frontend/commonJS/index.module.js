631c7599141afb8f19f6fece16e9a207c2632f6b
Anuarbek Zakiryanov <anuarbekzakirianov97@gmail.com>
2017-12-15 12:18:27 +0600

    commit

--- frontend/commonJS/index.module.js
+++ frontend/commonJS/index.module.js
@@ -1,2 +1,2 @@
-angular.module('myApp', ['ngAnimate','duScroll','ui.router','ngStorage','mgcrea.ngStrap','ui.bootstrap'])
+angular.module('myApp', ['ngAnimate','duScroll','ui.router','ngStorage','mgcrea.ngStrap'])
 
